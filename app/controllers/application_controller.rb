978e6c93ef38c746c7b1134ae3cba39a003972c6
purple-gecko <70636104+purple-gecko@users.noreply.github.com>
2020-11-27 17:40:20 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  # ユーザー認証
+  before_action :basic_auth
+
+  # 動作確認用アクション
+  def hello
+    render html: "hello, world!"
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
